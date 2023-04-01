3768c220cf5e146e6a10dce5ad5036e7a5a63cc7
Faberson Perfeito <faberson.perfeito@electrolux.com>
2023-04-01 15:53:38 -0300

    init card layout

--- SwiftUIDesignCodeiOS/ContentView.swift
+++ SwiftUIDesignCodeiOS/ContentView.swift
@@ -2,7 +2,7 @@
 // perfeito.dev
 //
 // ContentView.swift
-// SwiftUIDesignCodeiOS
+// SwiftUIDesignCodeiOSç
 //
 
 
@@ -10,10 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 0.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sectors - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and...")
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
         }
-        .padding()
+        .padding(.all, 20.0)
+        .frame(height: 350.0)
+        .background()
+        .cornerRadius(30.0)
+        .shadow(radius: 20)
     }
 }
 
