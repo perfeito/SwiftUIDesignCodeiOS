a7166bb5040c27a004b7579c5573a2c0cde6ed31
Faberson Perfeito <faberson.perfeito@electrolux.com>
2023-04-01 17:21:06 -0300

    Auto adaptive StrokeStyle in dark or white mode

--- SwiftUIDesignCodeiOS/Styles.swift
+++ SwiftUIDesignCodeiOS/Styles.swift
@@ -9,17 +9,23 @@
 import SwiftUI
 
 struct StrokeStyle: ViewModifier {
+    var cornerRadius: CGFloat
+    @Environment(\.colorScheme) var colorScheme
     func body(content: Content) -> some View {
         content.overlay(
-            RoundedRectangle(cornerRadius: 30, style: .continuous)
-                .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+            RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                .stroke(.linearGradient(
+                    colors: [
+                        .white.opacity(colorScheme == .dark ? 0.6 : 0.3),
+                        .black.opacity(colorScheme == .dark ? 0.3 : 0.1)
+                    ], startPoint: .top, endPoint: .bottom))
                 .blendMode(.overlay)
         )
     }
 }
 
 extension View {
-    func strokeStyle() -> some View {
-        modifier(StrokeStyle())
+    func strokeStyle(cornerRadius: CGFloat = 30) -> some View {
+        modifier(StrokeStyle(cornerRadius: cornerRadius))
     }
 }
