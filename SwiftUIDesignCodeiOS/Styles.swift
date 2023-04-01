6f63a8e7600dbf61741ff4d8039be63725c4132a
Faberson Perfeito <faberson.perfeito@electrolux.com>
2023-04-01 17:07:04 -0300

    Move StrokeStyle to own file

---
+++ SwiftUIDesignCodeiOS/Styles.swift
@@ -0,0 +1,25 @@
+//
+// perfeito.dev
+//
+// Styles.swift
+// SwiftUIDesignCodeiOS
+//
+
+
+import SwiftUI
+
+struct StrokeStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content.overlay(
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+                .blendMode(.overlay)
+        )
+    }
+}
+
+extension View {
+    func strokeStyle() -> some View {
+        modifier(StrokeStyle())
+    }
+}
