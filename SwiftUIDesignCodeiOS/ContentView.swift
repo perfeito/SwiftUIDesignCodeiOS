38a867e2efac59e1ae879029b7b0024a3b93384e
Faberson Perfeito <faberson.perfeito@electrolux.com>
2023-04-01 16:50:09 -0300

    upgrade card component

--- SwiftUIDesignCodeiOS/ContentView.swift
+++ SwiftUIDesignCodeiOS/ContentView.swift
@@ -2,9 +2,8 @@
 // perfeito.dev
 //
 // ContentView.swift
-// SwiftUIDesignCodeiOSç
+// SwiftUIDesignCodeiOS
 //
-
 
 import SwiftUI
 
@@ -16,26 +15,55 @@ struct ContentView: View {
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0)
+                .padding(9)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-            Text("20 sectors - 3 hours")
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            Text("20 sectors - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
             Text("Build an iOS app for iOS 15 with custom layouts, animations and...")
+                .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
         }
         .padding(.all, 20.0)
+        .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background()
-        .cornerRadius(30.0)
-        .shadow(radius: 20)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous)
+        )
+        //.cornerRadius(30.0)
+        //.mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+                .blendMode(.overlay)
+        )
+        .padding(.horizontal, 20)
+        .background(Image("Blob 1")
+            .offset(x: 250, y: -100)
+        )
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+        )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+            .previewDevice("iPhone SE (3rd generation)")
     }
 }
