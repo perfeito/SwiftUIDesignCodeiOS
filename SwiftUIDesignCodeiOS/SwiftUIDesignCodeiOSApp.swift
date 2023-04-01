474a55b78f71e3db0876e6b84677f047359ba148
Faberson Perfeito <fabersonperfeito@gmail.com>
2023-04-01 12:28:45 -0300

    Initial Commit

---
+++ SwiftUIDesignCodeiOS/SwiftUIDesignCodeiOSApp.swift
@@ -0,0 +1,19 @@
+//
+// perfeito.dev
+// PIX: perfeito.dev@gmail.com
+//
+// SwiftUIDesignCodeiOSApp.swift
+// SwiftUIDesignCodeiOS
+//
+
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignCodeiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
